ffbdce73dfe6758a25fce1a97e40af3136ea8f75
Sanjana-Saha <sanjana.saha@lntinfotech.com>
2020-09-27 14:38:36 +0530

    Sanjana First Commit

---
+++ BankingAngular/src/app/register-success/register-success.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-success',
+  templateUrl: './register-success.component.html',
+  styleUrls: ['./register-success.component.css']
+})
+export class RegisterSuccessComponent implements OnInit {
+
+  constructor() { }
+  goToLogin(){
+    
+  }
+  ngOnInit(): void {
+  }
+
+}
